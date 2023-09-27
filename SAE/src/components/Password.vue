b35e5059849d9751665a7749bb7cc647393f5823
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2023-09-27 17:23:58 +0200

    update

--- SAE/src/components/Password.vue
+++ SAE/src/components/Password.vue
@@ -1,43 +1,45 @@
 <template>
-  <h1>Mot de passe oublié : Coiffeton Shiny</h1>
-  <div v-if="codeCorrect">
-    <form @submit.prevent="changerPassword">
-      <p>Veuillez définir un nouveau mot de passe</p>
-      <div>
-        <label for="motDePasse">Mot de passe:</label>
-        <input type="password" id="motDePasse" v-model="utilisateur.motDePasse" required />
-      </div>
-      <div>
-        <label for="confirmationMotDePasse">Confirmation du mot de passe:</label>
-        <input type="password" id="confirmationMotDePasse" v-model="confirmationMotDePasse" required />
-      </div>
-      <button type="submit">Changer de mot de passe</button>
-    </form>
-  </div>
-  <div v-else>
-    <div v-if="codeRecive">
-      Veuillez indiquer le code reçu par mail :
-      <form @submit.prevent="acceptCode">
+  <div class="block" id="blockPassword">
+    <h1>MOT DE PASSE OUBLIE</h1>
+    <div v-if="codeCorrect">
+      <form @submit.prevent="changerPassword">
+        <p>Veuillez définir un nouveau mot de passe</p>
         <div>
-          <label for="code">Code:</label>
-          <input type="password" id="code" v-model="utilisateur.codeBase" required />
+          <label for="motDePasse">Mot de passe:</label>
+          <input type="password" id="motDePasse" v-model="utilisateur.motDePasse" required />
         </div>
-        <button type="submit">Vérifier le code</button>
+        <div>
+          <label for="confirmationMotDePasse">Confirmation du mot de passe:</label>
+          <input type="password" id="confirmationMotDePasse" v-model="confirmationMotDePasse" required />
+        </div>
+        <div id="divBoutonChangePassword" class="divBoutonPrincipal"><button type="submit" id="boutonChangePassword" class="boutonPrincipal"><span id="spanChangePassword" class="spanBoutonPrincipal">Changer de mot de passe</span> </button></div>
       </form>
     </div>
     <div v-else>
-      <div v-if="isAlreadyRegistered">
-        Vous êtes déjà connecté.
+      <div v-if="codeRecive">
+        Veuillez indiquer le code reçu par mail :
+        <form @submit.prevent="acceptCode">
+          <div>
+            <label for="code">Code:</label>
+            <input type="password" id="code" v-model="utilisateur.codeBase" required />
+          </div>
+          <div id="divBoutonVerifieCode" class="divBoutonPrincipal"><button type="submit" id="boutonVerifieCode" class="boutonPrincipal"><span id="spanVerifieCode" class="spanBoutonPrincipal">Vérifier le code</span> </button></div>
+        </form>
       </div>
       <div v-else>
-        <form @submit.prevent="demandeMotDePasse">
-        <div>
-          <label for="email">Adresse e-mail:</label>
-          <input type="email" id="email" v-model="utilisateur.email" required />
+        <div v-if="isAlreadyRegistered">
+          Vous êtes déjà connecté.
         </div>
-        <button type="submit">Envoyer un mail</button>
-        <p v-if="isAlreadyPressed">Veuillez patienter, votre demande de code est envoyé</p>
-        </form>
+        <div v-else>
+          <form @submit.prevent="demandeMotDePasse">
+          <div>
+            <label for="email">Adresse e-mail:</label>
+            <input type="email" id="email" v-model="utilisateur.email" required />
+          </div>
+          <div id="divBoutonEnvoieMail" class="divBoutonPrincipal"><button type="submit" id="boutonEnvoieMail" class="boutonPrincipal"><span id="spanEnvoieMail" class="spanBoutonPrincipal">Envoyer un mail</span> </button></div>
+          <p v-if="isAlreadyPressed">Veuillez patienter, votre demande de code est envoyé</p>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -179,4 +181,33 @@
   
   <style scoped>
     /* Styles pour l'en-tête */
+    #blockPassword{
+      margin-left: auto;
+      margin-right: auto;
+      border: 1px solid;
+      width: 600px;
+    }
+    /* .divBoutonPrincipal{
+  text-align: center;
+}
+
+.boutonPrincipal{
+  margin-top: 50px;
+  width: 40%;
+  height: 50px;
+  background-image: url("../assets/img/boutonCentral.png");
+  background-size: contain;
+  margin-bottom: 50px;
+  border-color: transparent;
+}
+
+.spanBoutonPrincipal{
+  font-family: 'Acme';
+  font-size: 30px;
+  text-shadow:
+  -1px 0px 0px white,
+  1px 0px 0px white,
+  0px -1px 0px white,
+  0px 1px 0px white;
+} */
   </style>
