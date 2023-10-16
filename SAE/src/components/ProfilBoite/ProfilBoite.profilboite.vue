be236a06e64af27f0c73b5515bc89b312936660e
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2023-10-16 08:52:33 +0200

    Update CSS

--- SAE/src/components/ProfilBoite/ProfilBoite.profilboite.vue
+++ SAE/src/components/ProfilBoite/ProfilBoite.profilboite.vue
@@ -1,12 +1,12 @@
 <template>
     <router-link :to="{ name: 'boite', params: { id_boite: boite.id_boite } }">
         <div class="boite">
-            <div class="image"><img :src="boite.lien_img_boi"></div>
-            <div class="nom">{{ boite.nom_boite }}</div>
-            <div class="numero">{{  boite.numero_boi }}</div>
-            <div class="univers">{{ boite.univers }}</div>
-            <div class="nombrePieces">{{ boite.nbr_pieceboi }}</div>
-            <div class="dateDeSortie">{{ boite.annee_sortie_boi }}</div>
+            <div class="image" id="imageProfilBoite"><img :src="boite.lien_img_boi"></div>
+            <div class="nom" id="nomProfilBoite"><p>{{ boite.nom_boite }}</p></div>
+            <div class="numero" id="numeroProfilBoite"><p>{{  boite.numero_boi }}</p></div>
+            <div class="univers" id="universProfilBoite"><p>{{ boite.univers }}</p></div>
+            <div class="nombrePieces" id="nombrePiecesProfilBoite"><p>{{ boite.nbr_pieceboi }}</p></div>
+            <div class="dateDeSortie" id="dateDeSortieProfilBoite"><p>{{ boite.annee_sortie_boi }}</p></div>
             <div>{{ boite.id_boite }}</div>
         </div>
     </router-link>
