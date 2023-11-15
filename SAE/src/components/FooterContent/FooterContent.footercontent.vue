88a60cc6c40e90aa34005a31eda4ad01ca1c30d1
UnderscoreXelven <enhance674@gmail.com>
2023-11-15 14:02:13 +0100

    update footer

--- SAE/src/components/FooterContent/FooterContent.footercontent.vue
+++ SAE/src/components/FooterContent/FooterContent.footercontent.vue
@@ -2,10 +2,12 @@
   <footer>
     <div id="texteFooter">
       <p class="textFooter">Pour nous contacter!</p>
-      <router-link to = "/contact" class="lien">Contact</router-link>  
-      <router-link to="/politique">Politique de confidentialité</router-link>
-      <router-link to="/cookies">Politique concernant les cookies</router-link>
-      <router-link to="/about">Mentions légales</router-link>
+      <router-link to = "/contact" class="lien">Contact</router-link> 
+      <div id="footer_line">
+        <router-link to="/politique" class="footer_info">Politique de confidentialité</router-link>
+        <router-link to="/cookies" class="footer_info">Politique concernant les cookies</router-link>
+        <router-link to="/about" class="footer_info">Mentions légales</router-link>
+      </div> 
     </div>
     
   </footer>
