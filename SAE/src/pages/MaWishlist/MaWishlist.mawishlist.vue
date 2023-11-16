3d166b1087172095bc46c7f8300fd755c896a23d
UnderscoreXelven <enhance674@gmail.com>
2023-11-16 17:20:08 +0100

    update

--- SAE/src/pages/MaWishlist/MaWishlist.mawishlist.vue
+++ SAE/src/pages/MaWishlist/MaWishlist.mawishlist.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="maWishlistDiv">
-      <h1>MA WISHLIST</h1>
+      <h1 id="titreMaWishlist">MA WISHLIST</h1>
       <Wishlist v-if="wishlist_id !== -1" :wishlist_id="wishlist_id"></Wishlist>
     </div>
 </template>
     
 <script src="./MaWishlist.mawishlist.js"></script>
-<style src="./MaWishList.mawishlist.scss"></style>
+<style src="./MaWishlist.mawishlist.scss"></style>
